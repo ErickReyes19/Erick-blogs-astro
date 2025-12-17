d86efef57afaf28c8e06c56ca9e7df5bea401626
ErickReyes19 <erickjosepineda33@gmail.com>
2025-12-17 11:56:39 -0600

    cambio en la variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site : 'https://example.com',
+  site : 'https://blog-astro-erick.netlify.app',
   vite: {
     plugins: [tailwindcss()]
   },
